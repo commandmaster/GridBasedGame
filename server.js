7206baa1faf3dce610ee3e3a75566da15c14ecf0
Bennett <153877155+commandmaster@users.noreply.github.com>
2024-04-15 14:11:38 -0600

    idek how to fix this

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
-const GPU = require('gpu.js');
+
 
 const app = express();
 const port = 3000;
@@ -441,40 +441,41 @@ io.on('connection', (socket) => {
     });
 });
 
+//const GPU = require('gpu.js');
 
-const getArrayValues = () => {
+// const getArrayValues = () => {
 
-    //Create 2D arrary here
-    const values = [[], []]
+//     //Create 2D arrary here
+//     const values = [[], []]
 
-    // Insert Values into first array
-    for (let y = 0; y < 600; y++){
-      values[0].push([])
-      values[1].push([])
+//     // Insert Values into first array
+//     for (let y = 0; y < 600; y++){
+//       values[0].push([])
+//       values[1].push([])
 
-      // Insert values into second array
-      for (let x = 0; x < 600; x++){
-        values[0][y].push(Math.random())
-        values[1][y].push(Math.random())
-      }
-    }
+//       // Insert values into second array
+//       for (let x = 0; x < 600; x++){
+//         values[0][y].push(Math.random())
+//         values[1][y].push(Math.random())
+//       }
+//     }
 
-    //Return filled array
-    return values
-  }
+//     //Return filled array
+//     return values
+//   }
 
-  const gpu = new GPU();
+//   const gpu = new GPU();
 
-  // Using `createKernel()` method to multiply the array
-  const multiplyLargeValues = gpu.createKernel(function(a, b) {
-    let sum = 0;
-    for (let i = 0; i < 600; i++) {
-      sum += a[this.thread.y][i] * b[i][this.thread.x];
-    }
-    return sum;
-  }).setOutput([600, 600])
+//   // Using `createKernel()` method to multiply the array
+//   const multiplyLargeValues = gpu.createKernel(function(a, b) {
+//     let sum = 0;
+//     for (let i = 0; i < 600; i++) {
+//       sum += a[this.thread.y][i] * b[i][this.thread.x];
+//     }
+//     return sum;
+//   }).setOutput([600, 600])
 
-  const largeArray = getArrayValues()
-  const out = multiplyLargeValues(largeArray[0], largeArray[1])
+//   const largeArray = getArrayValues()
+//   const out = multiplyLargeValues(largeArray[0], largeArray[1])
 
-    console.log(out)
+//     console.log(out)
